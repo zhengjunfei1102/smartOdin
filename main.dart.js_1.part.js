743129b886ab19cff598ba6f7d2b0921fd0e6772
郑俊飞 <15387037519@163.com>
2022-04-12 10:26:07 +0800

    初始化

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,10 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={OY:function OY(){}},B
+A=a.updateHolder(c[3],A)
+B=c[0]
+A.OY.prototype={
+t7(d){return"Hej "+d+"!"}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.OY,B.A)})()
+B.WM(b.typeUniverse,JSON.parse("{}"))}
+$__dart_deferred_initializers__["mME09IXlVe3OtIetWULKZGuZmK4="] = $__dart_deferred_initializers__.current
